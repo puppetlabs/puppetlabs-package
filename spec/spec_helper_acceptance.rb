80de0e4cafff529d785db9eab648f7c277f2483c
Hunter Haugen <hunter@puppet.com>
2017-08-31 17:27:29 -0700

    Add helpers, update code
    Changed:
    - split run_task to run_puppet_task and run_bolt_task
    
    Added:
    - add docker/pooler nodesets for other testing
    - make the tasks take params (not finished)
    
    Fixed:
    - Put tests in `it` blocks (they were naked and were unashamed)
    - update password handling for pooler
    - move the module to the environment location (see comments)
    - clean up setup helper code for future master/agent scenarios
    - fix the typing validation in status.json
    - various linting

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -10,8 +10,25 @@ install_module_dependencies_on(hosts)
 
 UNSUPPORTED_PLATFORMS = ['Windows', 'Solaris', 'AIX']
 
-def run_task(task_name:, args: nil)
-  on(default, "bolt run /etc/puppetlabs/code/modules/package/tasks/#{task_name} --nodes localhost --password vagrant", { :acceptable_exit_codes => [0,1] }).stdout
+DEFAULT_PASSWORD = if master[:hypervisor] == 'vagrant'
+                     'vagrant'
+                   elsif master[:hypervisor] == 'vcloud'
+                     '~!@#$%^*-/ aZ'
+                   end
+
+def run_puppet_access_login(user:, password: DEFAULT_PASSWORD, lifetime: "5y")
+  # FIXME: read user/password on stdin
+  on(master, puppet('access', 'login', user, password, '--lifetime', lifetime))
+end
+
+def run_bolt_task(module_name:, task_name:, params: nil, password: DEFAULT_PASSWORD)
+  on(master, "bolt run /etc/puppetlabs/code/environments/production/modules/#{module_name}/tasks/#{task_name} --nodes localhost --password #{password}", acceptable_exit_codes: [0, 1]).stdout
+end
+
+def run_puppet_task(task_name:, params: nil)
+  # FIXME: adreyer says "I thought the not passing environment bug was fixed" so --environment may not be needed
+  # FIXME: check how apply_manifest() does temp files for the params file
+  on(master, puppet('task', 'run', task_name, '--nodes', fact_on(master, 'fqdn'), '--environment', 'production', '--params-file', tempfile), acceptable_exit_codes: [0, 1]).stdout
 end
 
 RSpec.configure do |c|
@@ -20,20 +37,22 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
-    hosts.each do |host|
-      # fix the path
-      on(host, "echo PATH=\"$PATH:/opt/puppetlabs/puppet/bin\"  >> /etc/bash.bashrc")
-      
-      scp_to(default, "bolt-0.0.6.gem", '/tmp')
-pp=<<-EOS
-package { 'net-netconf' :
-  provider => 'puppet_gem',
-  ensure   => 'installed',
-  source   => '/tmp/bolt-0.0.6.gem'
-}
-EOS
-      create_remote_file(default, "/tmp/gems.pp", pp)
-      on host, puppet('apply','/tmp/gems.pp'), {:acceptable_exit_codes => [0,1] }
-    end
+    # fix the path
+    on(master, "echo PATH=\"$PATH:/opt/puppetlabs/puppet/bin\"  >> /etc/bash.bashrc")
+
+    scp_to(master, "bolt-0.0.6.gem", '/tmp')
+    pp = <<-EOS
+    package { 'net-netconf' :
+      provider => 'puppet_gem',
+      ensure   => 'installed',
+      source   => '/tmp/bolt-0.0.6.gem'
+    }
+    EOS
+    create_remote_file(hosts, '/tmp/gems.pp', pp)
+    on(hosts, puppet('apply', '/tmp/gems.pp'), acceptable_exit_codes: [0, 1])
+
+    run_puppet_access_login(user: 'admin', password: '~!@#$%^*-/ aZ')
+    # FIXME: adreyer says "it only supports [code/environments/production/modules] orch is reading it directly and has no access to modulepath"
+    on(master, 'mv /etc/puppetlabs/code/modules/package /etc/puppetlabs/code/environments/production/modules')
   end
 end
