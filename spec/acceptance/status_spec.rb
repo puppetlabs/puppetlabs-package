dedeae24ba38ecf5dd0435988a766b3c1aeee041
Hunter Haugen <hunter@puppet.com>
2017-09-07 08:17:34 -0700

    Remove shared example, update upgrade test
    The upgrade should work on multiple platforms

--- spec/acceptance/status_spec.rb
+++ spec/acceptance/status_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper_acceptance'
 
 describe 'package task' do
   describe 'install' do
+    before(:all) do
+      apply_manifest('package { "tmux": ensure => absent, }')
+    end
+
     it 'installs' do
       result = run_puppet_task(task_name: 'package', params: { 'action' => 'install', 'package' => 'tmux' })
       expect(result).to match(%r{status : installed})
@@ -11,48 +15,34 @@ describe 'package task' do
     end
   end
   describe 'uninstall' do
-    let(:pp) do
-      <<-EOS
-      package { 'tmux':
-        ensure => 'present',
-      }
-    EOS
+    before(:all) do
+      apply_manifest('package { "tmux": ensure => "present", }')
     end
 
-    it_behaves_like 'applies cleanly'
     it 'uninstalls' do
       result = run_puppet_task(task_name: 'package', params: { 'action' => 'uninstall', 'package' => 'tmux' })
       expect(result).to match(%r{status : uninstalled})
       expect(result).to match(%r{Job completed. 1/1 nodes succeeded})
     end
   end
-  describe 'upgrade', if: fact('operatingsystem') =~ %r{Redhat} do
-    let(:pp) do
-      <<-EOS
-      package { 'vim-minimal.x86_64':
-        ensure => '2:7.4.160-1.el7',
-      }
-    EOS
+  describe 'upgrade' do
+    before(:all) do
+      apply_manifest('package { "tmux": ensure => absent, }')
+      apply_manifest('package { "tmux": ensure => "1.7", }')
     end
 
-    it_behaves_like 'applies cleanly'
     it 'upgrades' do
-      result = run_puppet_task(task_name: 'package', params: { 'action' => 'upgrade', 'package' => 'vim-minimal.x86_64' })
-      expect(result).to match(%r{version : 2:7.4.160-1.el7_3.1})
+      result = run_puppet_task(task_name: 'package', params: { 'action' => 'upgrade', 'package' => 'tmux' })
+      expect(result).to match(%r{version : 1.8})
       expect(result).to match(%r{Job completed. 1/1 nodes succeeded})
     end
   end
   describe 'status' do
     context 'when package is present' do
-      let(:pp) do
-        <<-EOS
-        package { 'tmux':
-          ensure => 'present',
-        }
-      EOS
+      before(:all) do
+        apply_manifest('package { "tmux": ensure => "present", }')
       end
 
-      it_behaves_like 'applies cleanly'
       it 'returns the version' do
         result = run_puppet_task(task_name: 'package', params: { 'action' => 'status', 'package' => 'tmux' })
         expect(result).to match(%r{status : up to date})
@@ -60,6 +50,10 @@ describe 'package task' do
       end
     end
     context 'when package is absent' do
+      before(:all) do
+        apply_manifest('package { "tmux": ensure => "absent", }')
+      end
+
       it 'returns nothing' do
         result = run_puppet_task(task_name: 'package', params: { 'action' => 'status', 'package' => 'doesntexist' })
         expect(result).to match(%r{error : Tried to get latest on a missing package})
