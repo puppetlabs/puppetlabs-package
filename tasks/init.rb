59fe45f2ed860297baca7e1889235c3e06983c00
Hunter Haugen <hunter@puppet.com>
2017-09-21 08:16:25 -0700

    (maint) Sub pry for emacs, handle arrays of versions

--- tasks/init.rb
+++ tasks/init.rb
@@ -5,35 +5,35 @@ Puppet.initialize_settings
 require 'json'
 
 def install(provider, _version)
-  if [:absent, :purged].include?(provider.properties[:ensure])
+  if !([:absent, :purged] & Array(provider.properties[:ensure])).empty?
     provider.install
     provider.flush
-    { status: 'installed', version: provider.properties[:ensure] }
+    { status: 'installed', version: Array(provider.properties[:ensure]).join(", ") }
   else
-    { status: 'present', version: provider.properties[:ensure] }
+    { status: 'present', version: Array(provider.properties[:ensure]).join(", ") }
   end
 end
 
 def status(provider, _version)
-  version = provider.properties[:ensure]
-  if [:absent, :purged].include?(version)
+  version = Array(provider.properties[:ensure])
+  if !([:absent, :purged] & version).empty?
     { status: 'absent', version: version }
   else
     if provider.respond_to?(:latest)
       latest = provider.latest
-      if version != latest
-        { status: 'out of date', version: version, latest: latest }
+      if !version.include?(latest)
+        { status: 'out of date', version: version.join(", "), latest: latest }
       else
-        { status: 'up to date', version: version }
+        { status: 'up to date', version: version.join(", ") }
       end
     else
-      { status: 'unknown', version: version }
+      { status: 'unknown', version: version.join(", ") }
     end
   end
 end
 
 def uninstall(provider, _version)
-  if [:absent, :purged].include?(provider.properties[:ensure])
+  if !([:absent, :purged] & Array(provider.properties[:ensure])).empty?
     { status: 'absent' }
   else
     provider.uninstall
@@ -43,11 +43,11 @@ def uninstall(provider, _version)
 end
 
 def upgrade(provider, version)
-  old_version = provider.properties[:ensure]
+  old_version = Array(provider.properties[:ensure])
   provider.resource[:ensure] = version unless version.nil?
   provider.update
   provider.flush
-  { old_version: old_version, version: provider.properties[:ensure] }
+  { old_version: old_version.join(", "), version: Array(provider.properties[:ensure]).join(", ") }
 end
 
 params = JSON.parse(STDIN.read)
