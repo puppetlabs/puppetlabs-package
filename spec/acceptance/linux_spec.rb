1d787b6fc38414f6f7b0055e57273240c1e45693
lionce <42546087+lionce@users.noreply.github.com>
2019-03-13 16:35:07 +0200

    FM 7800 - Test Audit making testing more efficient (#116)
    * updated docs
    
    * task_run updates
    
    * removed facts
    
    * rubocop fixes

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -1,25 +1,17 @@
 # run a test task
 require 'spec_helper_acceptance'
 
-describe 'linux package task', unless: fact('osfamily') == 'windows' do
+describe 'linux package task', unless: os[:family] == 'windows' do
   include Beaker::TaskHelper::Inventory
   include BoltSpec::Run
 
-  let(:module_path) { RSpec.configuration.module_path }
-  let(:config) { { 'modulepath' => module_path } }
-  let(:inventory) { hosts_to_inventory }
-
-  def run(params)
-    run_task('package::linux', 'default', params, config: config, inventory: inventory)
-  end
-
-  redhat_six = fact('os.name') == 'RedHat' && fact('os.release.major') == '6'
-  windows = fact('osfamily') == 'windows'
+  redhat_six = os[:family] == 'redhat' && os[:release].to_i == 6
+  windows = os[:family] == 'windows'
 
   describe 'install action' do
     it 'install rsyslog', unless: redhat_six || windows do
       apply_manifest_on(default, "package { 'rsyslog': ensure => absent, }")
-      result = run('action' => 'install', 'name' => 'rsyslog')
+      result = task_run('package::linux', '', '', '', 'action' => 'install', 'name' => 'rsyslog')
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{install})
     end
@@ -28,16 +20,16 @@ describe 'linux package task', unless: fact('osfamily') == 'windows' do
   describe 'uninstall action', unless: redhat_six || windows do
     it 'uninstall rsyslog' do
       apply_manifest_on(default, "package { 'rsyslog': ensure => present, }")
-      result = run('action' => 'uninstall', 'name' => 'rsyslog')
+      result = task_run('package::linux', '', '', '', 'action' => 'uninstall', 'name' => 'rsyslog')
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{uninstall})
     end
   end
 
-  describe 'upgrade', if: (fact('operatingsystem') == 'CentOS' && fact('operatingsystemmajrelease') == '7') do
+  describe 'upgrade', if: (os[:family] == 'centos' && os[:release].to_i == 7) do
     it 'upgrade httpd' do
       apply_manifest_on(default, 'package { "httpd": ensure => "present", }')
-      result = run('action' => 'upgrade', 'name' => 'httpd')
+      result = task_run('package::linux', '', '', '', 'action' => 'upgrade', 'name' => 'httpd')
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{upgrade})
     end
