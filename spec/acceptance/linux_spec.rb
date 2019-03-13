257d267267f598d291e1ae3aec92b69e7541b29b
lionce <loredana.ionce@puppet.com>
2019-03-13 13:14:47 +0200

    removed facts

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -1,7 +1,7 @@
 # run a test task
 require 'spec_helper_acceptance'
 
-describe 'linux package task', unless: fact('osfamily') == 'windows' do
+describe 'linux package task', unless: os[:family] == 'windows' do
   include Beaker::TaskHelper::Inventory
   include BoltSpec::Run
 
@@ -11,7 +11,7 @@ describe 'linux package task', unless: fact('osfamily') == 'windows' do
   describe 'install action' do
     it 'install rsyslog', unless: redhat_six || windows do
       apply_manifest_on(default, "package { 'rsyslog': ensure => absent, }")
-      result = task_run('package::linux', 'action' => 'install', 'name' => 'rsyslog')
+      result = task_run('package::linux','', '', '', 'action' => 'install', 'name' => 'rsyslog')
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{install})
     end
@@ -20,16 +20,16 @@ describe 'linux package task', unless: fact('osfamily') == 'windows' do
   describe 'uninstall action', unless: redhat_six || windows do
     it 'uninstall rsyslog' do
       apply_manifest_on(default, "package { 'rsyslog': ensure => present, }")
-      result = task_run('package::linux', 'action' => 'uninstall', 'name' => 'rsyslog')
+      result = task_run('package::linux','','','', 'action' => 'uninstall', 'name' => 'rsyslog')
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{uninstall})
     end
   end
 
-  describe 'upgrade', if: (fact('operatingsystem') == 'CentOS' && fact('operatingsystemmajrelease') == '7') do
+  describe 'upgrade', if: (os[:family] == 'centos' && os[:release].to_i == 7) do
     it 'upgrade httpd' do
       apply_manifest_on(default, 'package { "httpd": ensure => "present", }')
-      result = task_run('package::linux', 'action' => 'upgrade', 'name' => 'httpd')
+      result = task_run('package::linux','','','', 'action' => 'upgrade', 'name' => 'httpd')
       expect(result[0]['status']).to eq('success')
       expect(result[0]['result']['status']).to match(%r{upgrade})
     end
