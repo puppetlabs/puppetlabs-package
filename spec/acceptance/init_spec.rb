513cc5aaf532dce69061da812b8dce5d328b2f7d
sheena <sheena@puppet.com>
2019-07-03 09:15:36 +0100

    FM-8303 adding acceptance tests in appveyor

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -4,9 +4,14 @@ require 'spec_helper_acceptance'
 describe 'package task' do
   operating_system_fact = os[:family]
   redhat_six = os[:family] == 'redhat' && os[:release].to_i == 6
+  target_host = ENV['TARGET_HOST']
 
   before(:all) do
-    inventory_hash = inventory_hash_from_inventory_file
+    inventory_hash = if target_host != 'localhost'
+                       inventory_hash_from_inventory_file
+                     else
+                       inventory_hash_from_inventory_file('spec/data/inventory.yaml')
+                     end
     inventory_hash = add_feature_to_group(inventory_hash, 'puppet-agent', 'ssh_nodes')
     inventory_hash = add_feature_to_group(inventory_hash, 'puppet-agent', 'winrm_nodes')
     inventory_hash = add_feature_to_group(inventory_hash, 'puppet-agent', 'docker_nodes')
