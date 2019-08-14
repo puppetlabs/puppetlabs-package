4a33af1fc47192124e20e90b0bc0457602311b29
sheena <sheena@puppet.com>
2019-08-14 16:09:12 +0100

    Remove the data file and update the tests

--- spec/acceptance/linux_spec.rb
+++ spec/acceptance/linux_spec.rb
@@ -15,7 +15,6 @@ describe 'linux package task', unless: redhat_six || windows do
                      end
     inventory_hash = add_feature_to_group(inventory_hash, 'puppet-agent', 'ssh_nodes')
     inventory_hash = add_feature_to_group(inventory_hash, 'puppet-agent', 'winrm_nodes')
-    inventory_hash = add_feature_to_group(inventory_hash, 'puppet-agent', 'docker_nodes')
     inventory_hash = add_feature_to_group(inventory_hash, 'puppet-agent', 'local')
     write_to_inventory_file(inventory_hash, 'inventory.yaml')
   end
