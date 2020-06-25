d11d396ae3fef971ad5ebecb666496f3197952bf
Filip Hrbek <filip.hrbek@gmail.com>
2020-06-25 15:39:49 +0200

    (IAC-908) Add support for package status in Windows

--- spec/acceptance/windows_spec.rb
+++ spec/acceptance/windows_spec.rb
@@ -51,22 +51,20 @@ PUPPETCODE
     end
   end
 
-  context 'when puppet-agent feature not available on target', if: target_host != 'localhost' && os[:family] == 'windows' do
-    it 'status action fails' do
-      inventory_hash = inventory_hash_from_inventory_file
-      inventory_hash = remove_feature_from_node(inventory_hash, 'puppet-agent', target_host)
-      inventory_hash = remove_feature_from_node(inventory_hash, 'puppet-agent', 'local')
-      temp_inventory_file = "#{ENV['TARGET_HOST']}.yaml"
-      write_to_inventory_file(inventory_hash, temp_inventory_file)
-      params = { 'action' => 'status', 'name' => package_to_use }
-      options = { inventory_file: temp_inventory_file }
-      expect { run_bolt_task('package', params, options) }.to raise_error(RuntimeError)
-      result = run_bolt_task('package', params, expect_failures: true, inventory_file: temp_inventory_file)
-      expect(result['result']).to include('status' => 'failure')
-      expect(result['result']['_error']).to include('msg' => %r{'status' action not supported})
-      expect(result['result']['_error']).to include('kind' => 'powershell_error')
-      expect(result['result']['_error']).to include('details')
-      File.delete(temp_inventory_file) if File.exist?(temp_inventory_file)
+  describe 'status action' do
+    it "status #{package_to_use} (installed)" do
+      apply_manifest("package { \"#{package_to_use}\": ensure => present, provider => 'chocolatey' }")
+      result = run_bolt_task('package::windows', 'action' => 'status', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('status' => %r{Installed})
+      expect(result['result']).to include('version')
+    end
+
+    it "status #{package_to_use} (uninstalled)" do
+      apply_manifest("package { \"#{package_to_use}\": ensure => absent, provider => 'chocolatey' }")
+      result = run_bolt_task('package::windows', 'action' => 'status', 'name' => package_to_use)
+      expect(result.exit_code).to eq(0)
+      expect(result['result']).to include('status' => %r{Uninstalled})
     end
   end
 end
