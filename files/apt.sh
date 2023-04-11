54c2939aa483848dfac9afe7bf04bcfc2e024a25
Romain Tartière <romain@blogreen.org>
2023-04-11 08:01:55 -1000

    Improve package status consistency on Debian
    In `dpkg-query(1)`, the `Status` field is composed of 3 values [source]:
    
    1. the package wanted status (e.g. `install`, `unknown`),
    2. the package status error flag (e.g. `ok`),
    3. the package status word (e.g. `installed`, `not-installed`).
    
    When requesting the status of a package, we have this kind of response:
    
    ```json
      {
        "status": "install ok installed",
        "version": "7.24.0-1bullseye"
      }
    ```
    
    A similar command targeting a CentOS node return a more consise status:
    
    ```json
      {
        "status": "installed",
        "version": "7.24.0-1.el7"
      }
    ```
    
    Hardcode the status value of installed packages to `installed` to match
    other operating systems.
    
    [source]:https://manpages.debian.org/bullseye/dpkg/dpkg-query.1.en.html

--- files/apt.sh
+++ files/apt.sh
@@ -18,9 +18,9 @@ apt_check_latest() {
   candidate="${candidate#*: }"
 
   if [[ $installed != $candidate ]]; then
-    cmd_status="$(apt_status "{ \"status\":\"\${Status}\", \"version\":\"${installed}\", \"latest\":\"${candidate}\" }")"
+    cmd_status="$(apt_status "{ \"status\":\"installed\", \"version\":\"${installed}\", \"latest\":\"${candidate}\" }")"
   else
-    cmd_status="$(apt_status '{ "status":"${Status}", "version":"${Version}" }')"
+    cmd_status="$(apt_status '{ "status":"installed", "version":"${Version}" }')"
   fi
 
   success "$cmd_status"
